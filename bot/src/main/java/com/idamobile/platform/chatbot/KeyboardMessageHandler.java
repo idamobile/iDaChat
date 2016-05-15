b9d6089061e1b4c974b8b70a3195e64861f679f3
zjor <zjor.se@gmail.com>
2016-05-15 14:33:34 +0200

    disabling bloken artifacts

--- bot/src/main/java/com/idamobile/platform/chatbot/KeyboardMessageHandler.java
+++ bot/src/main/java/com/idamobile/platform/chatbot/KeyboardMessageHandler.java
@@ -1,10 +1,7 @@
 package com.idamobile.platform.chatbot;
 
-import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
-import com.idamobile.platform.light.core.ws.client.WsEndpointClient;
-import com.idamobile.platform.light.core.ws.dto.WsCurrencyRateDTO;
-import com.idamobile.platform.light.core.ws.dto.contacts.WsContactDTO;
+import com.google.gson.reflect.TypeToken;
 import com.idamobile.platform.telegram.bot.api.dto.KeyboardButton;
 import com.idamobile.platform.telegram.bot.api.dto.Message;
 import com.idamobile.platform.telegram.bot.api.dto.SendMessageRequest;
@@ -13,13 +10,11 @@ import com.idamobile.platform.telegram.bot.framework.MessageHandlingCompletedExc
 import com.idamobile.platform.telegram.bot.framework.MessageHandlingFailedException;
 import org.apache.commons.lang3.StringUtils;
 
-import javax.inject.Inject;
-import java.text.MessageFormat;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
+
+//import com.idamobile.platform.light.core.ws.client.WsEndpointClient;
+//import com.idamobile.platform.light.core.ws.dto.WsCurrencyRateDTO;
+//import com.idamobile.platform.light.core.ws.dto.contacts.WsContactDTO;
 
 //TODO: it's a temporary stub keyboard handler
 //should be replaced with separate class per functionality
@@ -32,8 +27,8 @@ public class KeyboardMessageHandler implements MessageHandler {
     public static final String KEY_ATM = "Nearest ATM";
     public static final String I18N = "i18n";
 
-    @Inject
-    private WsEndpointClient client;
+//    @Inject
+//    private WsEndpointClient client;
 
     public static final KeyboardButton[][] KEYBOARD = new KeyboardButton[][]{
             {new KeyboardButton(KEY_NEWS), new KeyboardButton(KEY_CONTACTS)},
@@ -74,36 +69,36 @@ public class KeyboardMessageHandler implements MessageHandler {
     }
 
     private String getContacts() {
-        WsContactDTO[] cs = client.getContacts().getContacts();
-        if (cs == null || cs.length == 0) {
-            return "No contacts";
-        }
+//        WsContactDTO[] cs = client.getContacts().getContacts();
+//        if (cs == null || cs.length == 0) {
+//            return "No contacts";
+//        }
         StringBuilder contacts = new StringBuilder();
-        Arrays.stream(cs).forEach(c -> {
-            contacts.append(MessageFormat.format("[{0}] {1}: {2}", c.getType(), getLocalizedValue(c.getName()), getLocalizedValue(c.getValue())));
-            contacts.append('\n');
-        });
+//        Arrays.stream(cs).forEach(c -> {
+//            contacts.append(MessageFormat.format("[{0}] {1}: {2}", c.getType(), getLocalizedValue(c.getName()), getLocalizedValue(c.getValue())));
+//            contacts.append('\n');
+//        });
         return contacts.toString();
     }
 
     private String getExchangeRates() {
-        Map<String, List<WsCurrencyRateDTO>> index = new HashMap<>();
-        Map<String, String> names = new HashMap<>();
-        Arrays.stream(client.getCurrencyRates().getRates()).forEach(rate -> {
-            if (!index.containsKey(rate.getType())) {
-                index.put(rate.getType(), new LinkedList<>());
-                names.put(rate.getType(), getLocalizedValue(rate.getGroupName()));
-            }
-            index.get(rate.getType()).add(rate);
-        });
+//        Map<String, List<WsCurrencyRateDTO>> index = new HashMap<>();
+//        Map<String, String> names = new HashMap<>();
+//        Arrays.stream(client.getCurrencyRates().getRates()).forEach(rate -> {
+//            if (!index.containsKey(rate.getType())) {
+//                index.put(rate.getType(), new LinkedList<>());
+//                names.put(rate.getType(), getLocalizedValue(rate.getGroupName()));
+//            }
+//            index.get(rate.getType()).add(rate);
+//        });
         StringBuilder rates = new StringBuilder();
-        for (String type: index.keySet()) {
-            rates.append(names.get(type)).append('\n');
-            index.get(type).stream().forEach(r -> {
-                rates.append('\t').append(MessageFormat.format("{0}/{1} {2} {3}", r.getFirstCurrency(), r.getSecondCurrency(), r.getBuy(), r.getSell()));
-                rates.append('\n');
-            });
-        }
+//        for (String type: index.keySet()) {
+//            rates.append(names.get(type)).append('\n');
+//            index.get(type).stream().forEach(r -> {
+//                rates.append('\t').append(MessageFormat.format("{0}/{1} {2} {3}", r.getFirstCurrency(), r.getSecondCurrency(), r.getBuy(), r.getSell()));
+//                rates.append('\n');
+//            });
+//        }
         return rates.toString();
     }
 }
